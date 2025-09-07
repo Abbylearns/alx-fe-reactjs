809c8f07a3c95fb43c69cf9ad6a2a1b067b3762c
Abigael Okubi <abbiokubi@gmail.com>
2025-09-07 18:45:04 +0300

    Task 1: Create Counter component using useState and intergrate into App.jsx

---
+++ alx-react-app-new/src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Header from "./components/Header";
+import MainContent from "./components/MainContent";
+import UserProfile from "./components/UserProfile";
+import Footer from "./components/Footer";
+import Counter from "./components/Counter";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <MainContent />
+      <UserProfile />
+      <Counter />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
